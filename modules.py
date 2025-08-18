672b457f8f840d4af04af0433ea69a0d51a332d0
Zia Uddin <ziamaryam2526@gmail.com>
2025-08-18 05:09:11 -0700

    Added Modules File and Package

---
+++ modules.py
@@ -0,0 +1,6 @@
+from my_package import maths, science
+
+print(maths.add(10, 20))      # 30
+print(maths.sub(50, 15))      # 35
+print(science.gravity())      # "Gravity = 9.8 m/s^2"
+print(science.element())      # ['H', 'O', 'C']
