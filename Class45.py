2e9d78f16cc9bef0c725e1d59197c59f8bd72f26
ziauddin14 <ziamaryam2526@gmail.com>
2026-02-17 23:01:32 +0500

    data cleaning on movie dataset

--- Class45.py
+++ Class45.py
@@ -4,3 +4,37 @@ df = pd.read_csv('fue_station.csv')
 
 print(df.head(2))
 print(df.shape)
+
+# Kaunsa fuel sabse zyada hai?
+fuel_type = df['Fuel Type Code'].value_counts()
+print(fuel_type)
+# us fuel type ka naam 
+print(fuel_type.index[0])
+
+#Kaunsa fuel type konsy state mein sabse zyada hai?
+state_fuel = df.groupby('State')['Fuel Type Code'].value_counts()
+print(state_fuel)
+
+# Kis state mein sabse zyada stations hain? + us state ka naam 
+state = df['State'].value_counts()
+print(state.index[0])
+
+# Public stations kitni hain?
+public_stations = df[df['Groups With Access Code'] == 'Public']
+print(public_stations.shape[0])
+
+#kaunsy state mein sabse zyada public stations hain? 
+public_state = df[df['Groups With Access Code'] == 'Public']['State'].value_counts()
+print(public_state.index[0]) 
+    
+# EV stations kitni hain?
+ev_stations = df[df['Fuel Type Code'] == 'ELEC']
+print(ev_stations.shape[0])
+
+# Kitni stations 24/7 hain?
+twenty_four_seven_stations = df[df['Access Days Time'] == '24 hours daily']
+print(twenty_four_seven_stations.shape[0])
+
+# Kitni stations last 5 years mein update hui hain?
+recent_stations = df[df['Updated At'] >= '2022-01-01']
+print(recent_stations.shape[0])
---
+++ Class46.py
@@ -0,0 +1,38 @@
+import pandas as pd 
+import numpy as np
+import matplotlib as plt
+
+df = pd.read_csv('movie.csv')
+
+#----------------------Data Cleaning -----------------------#
+
+print(df.head())
+print(df['Runtime'].head())
+print(df['Runtime'].dtype)
+df['Runtime'] = df['Runtime'].replace('min', '')
+df['Runtime'] = pd.to_numeric(df['Runtime'], errors='coerce')
+print(df['Runtime'])
+
+# change Released Year type
+df['Released_Year'] = pd.to_numeric(df['Released_Year'], errors='coerce')
+print(df['Released_Year'].dtype)
+print(df.info())
+
+print(df.isnull().sum())
+
+#fill the null values with median
+df['Meta_score'] = df['Meta_score'].fillna(df['Meta_score'].median())
+print(df['Meta_score'].isnull().sum())
+df['Gross'] = df['Gross'].fillna(df['Gross'].median())
+print(df.isnull().sum())
+df['Certificate'] = df['Certificate'].fillna(df['Certificate'].mode()[0])
+print(df.isnull().sum())
+
+#----------IMDB rating distribution------------------#
+plt.figure(figsize=(8, 6))
+plt.hist(df['IMDB_Rating'], bins=20, color='blue', edgecolor='black')
+plt.title('Distribution of IMDB Ratings')
+plt.xlabel('IMDB Rating')
+plt.ylabel('Frequency')
+plt.grid(axis='y', alpha=0.75)
+plt.show()
