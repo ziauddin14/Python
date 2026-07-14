dd178120eed57de2512646e7b96078841541fee9
ziauddin14 <ziamaryam2526@gmail.com>
2026-07-14 22:27:17 +0500

    feat: add documentation module explaining RNN types and architecture

---
+++ Class72.py
@@ -0,0 +1,52 @@
+'''
+
+RNN : Recurrent Neural Network
+
+Types of RNN :
+    1. Vanilla RNN
+    2. LSTM (Long Short Term Memory)
+    3. GRU (Gated Recurrent Unit)
+
+1. Vanilla RNN:
+    It is the simplest form of RNN.
+    It is used for sequential data.
+    It is used for time series data.
+    It is used for natural language processing.
+    It is used for speech recognition.
+
+2. LSTM:
+    It is an improvement over vanilla RNN.
+    It is used for sequential data.
+    It is used for time series data.
+    It is used for natural language processing.
+    It is used for speech recognition.
+    It is used for image recognition.
+
+3. GRU:
+    It is an improvement over LSTM.
+    It is used for sequential data.
+    It is used for time series data.
+    It is used for natural language processing.
+    It is used for speech recognition.
+    It is used for image recognition.
+
+
+Architecture of RNN :
+    1. Input Layer
+    2. Hidden Layer
+    3. Output Layer
+    
+
+1. Input Layer : 
+    It is used to receive the input data.
+
+2. Hidden Layer : 
+    It is used to process the input data.
+
+3. Output Layer : 
+    It is used to output the result.
+
+
+Hidden State: It is a vector that is used to store the information of the previous hidden state.
+
+'''
